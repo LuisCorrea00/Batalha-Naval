fa4fc47f9a13acb325a08e556badef1c37278843
Luis Correa <henriqueluis785@gmail.com>
2023-06-23 01:05:29 -0300

    fixing and refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static void alocarNavio(int disp, int tipoNavio, Player player) throws InterruptedException {
+        boolean flagEnd = false;
+        int coluna = 0;
+        Scanner ler = new Scanner(System.in);
+        do {
+            if (disp!=0) { // verifica se ainda tem barcos disponiveis
+                System.out.println();
+                System.out.println("Escolha a localização do navio");
+                player.getMap();
+                System.out.print("Linha: ");
+                int linha = ler.nextInt();
+                System.out.print("Coluna: ");
+                char colunaChar = ler.next().charAt(0);
+                colunaChar = Character.toUpperCase(colunaChar);
+                for (int i = 0; i<player.coluna.length;i++){
+                    if (colunaChar == player.coluna[i]) coluna = i;
+                }
+                System.out.println("Escolha a posição do návio");
+                System.out.println("Horizontal [1]  ||  Vertical [2]");
+                System.out.print("Selecione: ");
+                int posicao = ler.nextInt();
+                if (player.setNavio(linha, coluna, posicao, tipoNavio)) { // se atribuir corretamente retorna true
+                    flagEnd = true; // encerra o loop
+                } else { // se houver algum erro retorna false e retoma o loop
+                    System.out.println("Localização Inválida!!");
+                    Thread.sleep(1000); // pausa o prompt
+                }
+            }
+            else{
+                System.out.println("Você não tem mais navios disponíveis!"); // se n houver barcos disponiveis retoma o loop
+                flagEnd = true;
+                Thread.sleep(1000); // pausa o prompt
+            }
+        }while (!flagEnd);
+    }
+
     public static void main(String[] args) throws InterruptedException {
         Scanner ler = new Scanner(System.in);
         System.out.println("------ BATALHA NAVAL ------ ");
@@ -14,8 +51,6 @@ public class Main {
                 int navioG = 2;
                 int navioM = 3;
                 int navioP = 4;
-                int linha, coluna, posicao;
-                int flagEnd; // flag para parar de repetir o laço
                 System.out.println("------ MODO SINGLE PLAYER ------ ");
                 Player player1 = new Player(); //cria o jogador
                 Player bot = new Player(); // cria o bot
@@ -31,7 +66,7 @@ public class Main {
                 System.out.println("Selecione: ");
                 int opcao = ler.next().charAt(0);
                 switch (opcao){
-                    case 'm', 'M' ->{
+                    case 'm', 'M' ->{ // manualmente
                         do {
                             System.out.println();
                             System.out.println("Escolha o tipo de navio para posicionar");
@@ -43,136 +78,36 @@ public class Main {
                             int tipoNavio = ler.nextInt();
                             switch (tipoNavio){
                                 case 4 ->{
-                                    flagEnd = 0;
-                                    do {
-                                        if (navioGG!=0) { // verifica se ainda tem barcos disponiveis
-                                            System.out.println();
-                                            System.out.println("Escolha a localização do navio");
-                                            player1.getMap();
-                                            System.out.print("Linha: ");
-                                            linha = ler.nextInt();
-                                            System.out.print("Coluna: ");
-                                            coluna = ler.nextInt();
-                                            System.out.println("Escolha a posição do návio");
-                                            System.out.println("Horizontal [1]  ||  Vertical [2]");
-                                            System.out.print("Selecione: ");
-                                            posicao = ler.nextInt();
-                                            if (player1.setNavioGG(linha, coluna, posicao)) { // se atribuir corretamente retorna true
-                                                navioGG--; // diminui os barcos disponiveis
-                                                flagEnd = 1; // encerra o loop
-                                            } else { // se houver algum erro retorna false e retoma o loop
-                                                System.out.println("Localização Inválida!!");
-                                                Thread.sleep(1000); // pausa o prompt
-                                            }
-                                        }
-                                        else{
-                                            System.out.println("Você não tem mais navios disponíveis!"); // se n houver barcos disponiveis retoma o loop
-                                            flagEnd = 1;
-                                            Thread.sleep(1000); // pausa o prompt
-                                        }
-                                    }while (flagEnd == 0);
+                                    alocarNavio(navioGG,tipoNavio,player1);
+                                    if (navioGG != 0 ) navioGG--;
                                 }
                                 case 3 ->{
-                                    flagEnd = 0;
-                                    do {
-                                        if (navioG!=0) {
-                                            System.out.println();
-                                            System.out.println("Escolha a localização do navio");
-                                            player1.getMap();
-                                            System.out.print("Linha: ");
-                                            linha = ler.nextInt();
-                                            System.out.print("Coluna: ");
-                                            coluna = ler.nextInt();
-                                            System.out.println("Escolha a posição do návio");
-                                            System.out.println("Horizontal [1]  ||  Vertical [2]");
-                                            System.out.print("Selecione: ");
-                                            posicao = ler.nextInt();
-                                            if (player1.setNavioG(linha, coluna, posicao)) {
-                                                navioG--;
-                                                flagEnd = 1;
-                                            } else {
-                                                System.out.println("Localização Inválida!!");
-                                                Thread.sleep(1000);
-                                            }
-                                        }
-                                        else{
-                                            System.out.println("Você não tem mais navios disponíveis!");
-                                            flagEnd = 1;
-                                            Thread.sleep(1000);
-                                        }
-                                    }while (flagEnd == 0);
+                                    alocarNavio(navioG,tipoNavio,player1);
+                                    if (navioG != 0 ) navioG--;
                                 }
                                 case 2 ->{
-                                    flagEnd = 0;
-                                    do {
-                                        if (navioM!=0) {
-                                            System.out.println();
-                                            System.out.println("Escolha a localização do navio");
-                                            player1.getMap();
-                                            System.out.print("Linha: ");
-                                            linha = ler.nextInt();
-                                            System.out.print("Coluna: ");
-                                            coluna = ler.nextInt();
-                                            System.out.println("Escolha a posição do návio");
-                                            System.out.println("Horizontal [1]  ||  Vertical [2]");
-                                            System.out.print("Selecione: ");
-                                            posicao = ler.nextInt();
-                                            if (player1.setNavioM(linha, coluna, posicao)) {
-                                                navioM--;
-                                                flagEnd = 1;
-                                            } else {
-                                                System.out.println("Localização Inválida!!");
-                                                Thread.sleep(1000);
-                                            }
-                                        }
-                                        else{
-                                            System.out.println("Você não tem mais navios disponíveis!");
-                                            flagEnd = 1;
-                                            Thread.sleep(1000);
-                                        }
-                                    }while (flagEnd == 0);
+                                    alocarNavio(navioM,tipoNavio,player1);
+                                    if (navioM != 0 ) navioM--;
                                 }
                                 case 1 ->{
-                                    flagEnd = 0;
-                                    do {
-                                        if (navioP!=0) {
-                                            System.out.println();
-                                            System.out.println("Escolha a localização do navio");
-                                            player1.getMap();
-                                            System.out.print("Linha: ");
-                                            linha = ler.nextInt();
-                                            System.out.print("Coluna: ");
-                                            coluna = ler.nextInt();
-                                            System.out.println("Escolha a posição do návio");
-                                            System.out.println("Horizontal [1]  ||  Vertical [2]");
-                                            System.out.print("Selecione: ");
-                                            posicao = ler.nextInt();
-                                            if (player1.setNavioP(linha, coluna, posicao)) {
-                                                navioP--;
-                                                flagEnd = 1;
-                                            } else {
-                                                System.out.println("Localização Inválida!!");
-                                                Thread.sleep(1000);
-                                            }
-                                        }
-                                        else{
-                                            System.out.println("Você não tem mais navios disponíveis!");
-                                            flagEnd = 1;
-                                            Thread.sleep(1000);
-                                        }
-                                    }while (flagEnd == 0);
+                                    alocarNavio(navioP,tipoNavio,player1);
+                                    if (navioP != 0 ) navioP--;
                                 }
                                 default -> System.out.println("opção Inválida!");
                             }
                         }while (navioGG != 0 || navioG != 0 || navioM != 0 || navioP != 0); // se nao houver mais barcos disponiveis encerra a atribuicao
-                        bot.getMap(); // provisorio
+//                        bot.getMap(); // provisorio
                     }
-                    case 'a','A' -> {
+                    case 'a','A' -> { //automaticamente
                         player1.setMapRandom(); //define aleatoriamente o mapa do jogador
-                        player1.getMap(); // provisorio
-                        bot.getMap(); // provisorio
+//                        player1.getMap(); // provisorio
+//                        bot.getMap(); // provisorio
                     }
                 }
+                System.out.println("------ HORA DE ATACAR ------");
+                System.out.println();
+                System.out.println("------ SEU MAPA ------");
+                player1.getMap();
 
             }
             case 'm','M' -> {
--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,7 @@ public class Player {
 
     String nome;
     char[][] map = new char[10][10];
+    char[] coluna = {'A','B','C','D','E','F','G','H','I','J'};
 
     public void setName(String nome){ //define o nome do jogador
         this.nome = nome;
@@ -19,15 +20,19 @@ public class Player {
         }
     }
 
-    public boolean setNavioGG(int linha, int coluna, int posicao){ //define navio de 4 espacos
+    public boolean setNavio(int linha, int coluna, int posicao, int navio){ //define navio no mapa
+        int colInicio = coluna;
+        int linInicio = linha;
         for (int l=0, tamanho = 1; l<map.length; l++){
             for (int c=0; c<map[0].length;c++){
                 if (l==linha && c==coluna){ //se a linha e coluna definidas pelo usuario coincidem
                     if (posicao == 1){ //se o usuario escolheu horizontal
-                        if(coluna+3 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
+                        if(colInicio+navio <= map.length){ //verifica se tem espaco para alocar o navio na matriz
                             if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
                                 map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 4) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
+                                System.out.println(navio);
+                                System.out.println("x");
+                                if (tamanho < navio) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
                                     coluna++;
                                     tamanho++;
                                 }
@@ -41,137 +46,13 @@ public class Player {
                         }
                     }
                     else if (posicao == 2) { //se o usuario escolheu vertical
-                        if(linha+3 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
+                        if(linInicio+navio <= map.length){ //verifica se tem espaco para alocar o navio na matriz
                             if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
                                 map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 4) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
+                                if (tamanho < navio) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
                                     linha++;
                                     tamanho++;
                                 }
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean setNavioG(int linha, int coluna, int posicao){
-        for (int l=0, tamanho = 1; l<map.length; l++){
-            for (int c=0; c<map[0].length;c++){
-                if (l==linha && c==coluna){ //se a linha e coluna definidas pelo usuario coincidem
-                    if (posicao == 1){ //se o usuario escolheu horizontal
-                        if(coluna+2 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
-                                map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 3) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
-                                    coluna++;
-                                    tamanho++;
-                                }
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                    else if (posicao == 2) { //se o usuario escolheu vertical
-                        if(linha+2 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') {
-                                map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 3) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
-                                    linha++;
-                                    tamanho++;
-                                }
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean setNavioM(int linha, int coluna, int posicao){
-        for (int l=0, tamanho = 1; l<map.length; l++){
-            for (int c=0; c<map[0].length;c++){
-                if (l==linha && c==coluna){ //se a linha e coluna definidas pelo usuario coincidem
-                    if (posicao == 1){ //se o usuario escolheu horizontal
-                        if(coluna+1 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
-                                map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 2) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
-                                    coluna++;
-                                    tamanho++;
-                                }
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                    else if (posicao == 2) { //se o usuario escolheu vertical
-                        if(linha+1 <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
-                                map[l][c] = 'B'; //define o navio na matriz
-                                if (tamanho < 2) { //verifica se ainda deve definir o navio no mapa dependendo do tamanho dele
-                                    linha++;
-                                    tamanho++;
-                                }
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean setNavioP(int linha, int coluna, int posicao){
-        for (int l=0; l<map.length; l++){
-            for (int c=0; c<map[0].length;c++){
-                if (l==linha && c==coluna){ //se a linha e coluna definidas pelo usuario coincidem
-                    if (posicao == 1){ //se o usuario escolheu horizontal
-                        if(coluna <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
-                                map[l][c] = 'B'; //define o navio na matriz
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false; //se n tiver espaco encerra
-                        }
-                    }
-                    else if (posicao == 2) { //se o usuario  escolheu vertical
-                        if(linha <= map.length){ //verifica se tem espaco para alocar o navio na matriz
-                            if (map[l][c] != 'B') { //verifica se tem um navio ocupando o espaco
-                                map[l][c] = 'B'; //define o navio na matriz
                             }
                             else{
                                 return false;
@@ -195,22 +76,22 @@ public class Player {
         int navioP = 4;
         do {
             if (navioGG!=0){ // se ainda tiver barcos disponiveis
-                if (setNavioGG(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2))){ // se a atribuicao do barco funcionar, retorna true
+                if (setNavio(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2),4)){ // se a atribuicao do barco funcionar, retorna true
                     navioGG--; // diminui 1 barco dos disponiveis
                 }
             }
             if (navioG!=0){
-                if (setNavioG(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2))){
+                if (setNavio(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2),3)){
                     navioG--;
                 }
             }
             if (navioM!=0){
-                if (setNavioM(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2))){
+                if (setNavio(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2),2)){
                     navioM--;
                 }
             }
             if (navioP!=0){
-                if (setNavioP(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2))){
+                if (setNavio(aleatorio.nextInt(1,9), aleatorio.nextInt(1,9), aleatorio.nextInt(1,2),1)){
                     navioP--;
                 }
             }
@@ -219,8 +100,12 @@ public class Player {
     }
 
     public void getMap(){ // imprime o mapa atualizado do jogador
-        System.out.println("    0   1   2   3   4   5   6   7   8   9  ");
         int count= 0; //contador para ver se tem todos os 20 barcos no mapa || 🚨🚨 está atribuindo mais que 20 🚨🚨
+        for (int i = 0; i<coluna.length;i++){
+            if (i==0) System.out.print("    "+coluna[i]+"   ");
+            else System.out.print(coluna[i]+"   ");
+        }
+        System.out.println();
         for (int l = 0; l<map.length; l++){
             for (int c = 0; c<map[0].length;c++){
                 if (c==0){
